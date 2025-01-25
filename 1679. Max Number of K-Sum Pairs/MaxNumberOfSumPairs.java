aef63033cf3bf3ec289494892558e04f260d3151
Leonardo Farias <leofariasrj25@gmail.com>
2025-01-25 07:23:59 -0300

    1679. Max Number of K-Sum Pairs (java ver)

---
+++ 1679. Max Number of K-Sum Pairs/MaxNumberOfSumPairs.java
@@ -0,0 +1,22 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class MaxNumberOfSumPairs {
+	public int maxOperations(int[] nums, int k) {
+		Map<Integer, Integer> freqMap = new HashMap<>();
+		int operations = 0;
+
+		for (int num : nums) {
+			int complement = k - num;
+
+			if (freqMap.getOrDefault(complement, 0) > 0) {
+				operations++;
+				freqMap.put(complement, freqMap.get(complement) - 1);
+			} else {
+				freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+			}
+		}
+
+		return operations;
+	}
+}
